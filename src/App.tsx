8820215f229e0efc70f560f6f5139c7188ebeb8e
Safnaj <safnaj99@gmail.com>
2022-11-26 17:54:10 +0530

    Proect Init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'></header>
+    </div>
+  );
+}
+
+export default App;
